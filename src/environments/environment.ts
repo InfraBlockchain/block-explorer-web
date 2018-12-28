8de6619f06c185460a3ab81b154c561dfe04646a
bezalel <bezalel.dev@gmail.com>
2018-12-28 12:01:11 +0900

    envionment config for YOSEMITE Sentinel Testnet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,20 @@
 export const environment = {
   production: false,
   appName: 'YOSEMITE Blockchain Explorer',
-  networkName: 'Public Test-Net',
+  networkName: 'Sentinel Public Test-Net',
   logoUrl: '/assets/logo.png',
-  apiUrl: 'http://localhost:9000',
-  blockchainUrl: 'http://localhost:8888',
-  chainId: '6376573815dbd2de2d9929027a94aeab3f6e60e87caa953f94ee701ac8425811',
+
+  // local-testnet-sentinel
+  // apiUrl: 'http://localhost:9000',
+  // blockchainUrl: 'http://localhost:8888',
+  // chainId: '047316f411b2db9ba0f600fdbca8e3bbd224d82a367ff02fbd355bb0675288e3',
+
+  // testnet-sentinel
+  apiUrl: 'http://testnet-sentinel-explorer-api.yosemitelabs.org',
+  blockchainUrl: 'http://testnet-sentinel.yosemitelabs.org:8888',
+  chainId: '047316f411b2db9ba0f600fdbca8e3bbd224d82a367ff02fbd355bb0675288e3',
+
+  //// testnet-v1
   // apiUrl: 'http://testnet-explorer-api.yosemitelabs.org',
   // blockchainUrl: 'http://testnet.yosemitelabs.org:8888',
   // chainId: '6376573815dbd2de2d9929027a94aeab3f6e60e87caa953f94ee701ac8425811',
